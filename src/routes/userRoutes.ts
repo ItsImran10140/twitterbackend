4b640e2ada35c5bdc253c4705b9373ceb02454cd
Imran Shah <imranshah10140@gmail.com>
2024-10-05 02:47:43 +0530

    Backend Connected and user creation functionality POST resquest

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,22 +1,38 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+
 const router = Router();
+const prisma = new PrismaClient();
 
 // user create
-router.post("/", (req, res) => {
-  res.status(501).json({ error: "No Implemented" });
+router.post("/", async (req, res) => {
+  const { email, name, username } = req.body;
+  console.log(email, name, username);
+  const result = await prisma.user.create({
+    data: {
+      email,
+      name,
+      username,
+      bio: "Hello, I'm new on twitter",
+    },
+  });
+  res.json(result);
 });
 // List users
-router.get("/", (req, res) => {
-  res.status(501).json({ error: "No Implemented" });
+router.get("/", async (req, res) => {
+  const allUser = await prisma.user.findMany();
+  res.json(allUser);
 });
 // get one user
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `No Implemented ${id}` });
+  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+  res.json(user);
 });
 // update user
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
+
   res.status(501).json({ error: `No Implemented ${id}` });
 });
 // delete user
